dd2edebab2e23805f8071f67ff1c3ee22a5a3452
Chelsea Finn <cfinnswim@gmail.com>
2017-02-16 18:53:13 -0800

    updated bug fix

--- miniImagenet/resize_images.py
+++ miniImagenet/resize_images.py
@@ -9,7 +9,7 @@ i = 0
 
 for image_file in all_images:
     im = Image.open(image_file)
-    im.resize((84,84), resample=Image.LANCZOS)
+    im = im.resize((84,84), resample=Image.LANCZOS)
     im.save(image_file)
     i += 1
 
